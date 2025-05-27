101cae5897a4198a34bdd4b59344be061d118167
victor esemann lunelli <lunellivictor950@gmail.com>
2025-05-27 11:15:06 -0300

    Add files via upload

--- site/cadastrar.php
+++ site/cadastrar.php
@@ -2,9 +2,25 @@
     $nome = $_POST["nome"];
     $cpf = $_POST["cpf"];
     $cep = $_POST["cep"];
+    $endereco =  $_POST["endereco"];
+    $numero_da_casa = $_POST["numero_da_casa"];
+    $referencia = $_POST["referencia"];
 
     echo("seja bem-vindo, $nome!");
-    echo("seu cpf, $cpf");
-    echo("seu cep, $cep");
+    echo("<br>");
+    echo("confira seus dados");
+    echo("<br>");
+    echo("<br>");
+    echo("cpf: $cpf");
+    echo("<br>");
+    echo("cep: $cep");
+    echo("<br>");
+    echo("endereço: $endereco");
+    echo("<br>");
+    echo("n° da casda: $numero_da_casa");
+    echo("<br>");
+    echo("referencia: $referencia");
+    echo("<BR>");
+    echo("bem vindo ao nosso site");
 
 ?>
